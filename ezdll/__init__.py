7642635eca5fff55f5dcbdff883924e36e590a45
hc07180011 <henrychao0718@gmail.com>
2020-11-16 23:56:44 +0800

    PyPI settings

---
+++ ezdll/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import numpy as np
+
+
+__author__ = 'Henry Chao'
+__copyright__ = 'Copyright 2020, The NTU Techsol'
+__credits__ = ['Cupid Huang', 'Ray Ho', 'Yunchia Pig']
+__license__ = 'Apache License 2.0'
+__version__ = '0.0.1'
+__maintainer__ = 'Henry Chao'
+__email__ = 'techsol.workspace.tmp@gmail.com'
+__status__ = 'Dev'
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='easy-cdll',
+    version='0.0.2',
+    author='techsol-ntu',
+    author_email='techsol.workspace.tmp@gmail.com',
+    description='Boost the transaction formatting and development between python and c languages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/techsol-ntu/easy-cdll',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.3',
+)
