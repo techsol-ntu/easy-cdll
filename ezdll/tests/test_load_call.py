bcfdc7f208f6f6682ad924dfb389230f1dcf29db
hc07180011 <henrychao0718@gmail.com>
2020-11-18 05:19:29 +0800

    try to pass test Q

--- ezdll/tests/test_load_call.py
+++ ezdll/tests/test_load_call.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import glob
 import numpy as np
 from ezdll.cstyles import cstyles
 
@@ -8,12 +9,15 @@ def test_platform():
     assert sys.platform == 'linux' or sys.platform == 'darwin'
 
 def test_compile():
-    if not os.path.exists('libtest.so'):
-        os.system('g++ -std=c++17 -shared -fPIC -o libtest.so test.cpp')
-    a = cstyles.Cdll(os.path.join('.', 'libtest.so'))
+    dll_path = glob.glob('./**/libtest.so', recursive = True)
+    if not len(dll_path):
+        tests_path = glob.glob('./**/tests/', recursive = True)[0]
+        os.system('g++ -std=c++17 -shared -fPIC -o {} {}'.format(os.path.join(tests_path, 'libtest.so'), os.path.join(tests_path, 'test.cpp')))
+    dll_path = glob.glob('./**/libtest.so', recursive = True)
+    a = cstyles.Cdll(dll_path[0])
     raw_data = {'a': [1,], 'b': [2, 3], 'c': [[4, 5], [6, 7]]}
     results = a.call_function('api', raw_data)
-    os.remove('libtest.so')
+    os.remove(dll_path[0])
     with open('test_output.txt', 'r') as f:
         buf = f.read()
     os.remove('test_output.txt')
--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.4',
 )
